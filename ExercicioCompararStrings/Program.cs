31603eac2625bcd02ace8b61afda268260297d77
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-08 01:40:06 -0300

    Add project files.

---
+++ ExercicioCompararStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿string primeira, segunda;
+
+Console.WriteLine("Digite a primeira string:");
+primeira = Console.ReadLine();
+
+Console.WriteLine("Digite a segunda string:");
+segunda = Console.ReadLine();
+
+if (primeira.Equals(segunda))
+{
+    Console.WriteLine("As strings são iguais");
+}
+else
+    Console.WriteLine("As strings NÃO são iguais");
